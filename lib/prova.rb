756ac5cc178f9a027bbab0266a9d2e109691eff6
Tony <tony.fusco.85@gmail.com>
2022-05-29 18:31:37 +0200

    init

---
+++ lib/prova.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "prova/version"
+
+module Prova
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/prova/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Prova
+  VERSION = "0.1.0"
+end
